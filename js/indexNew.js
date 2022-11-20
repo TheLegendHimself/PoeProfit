536bec693da88ee856d477face9e43ad85fc6c31
TheLegendHimself <58825123+TheLegendHimself@users.noreply.github.com>
2022-11-20 04:16:30 +0100

    added newHtml

---
+++ js/indexNew.js
@@ -0,0 +1,24 @@
+// atlasState 1 = True ; 0 = False
+var atlasState = 1;
+var uberState = 1;
+
+function changeBtnState(whatButton){
+	if(whatButton=="Atlas"){
+		if(atlasState == 0){
+			atlasState = 1;
+		}else{
+			atlasState = 0;
+		}	
+	}
+	if(whatButton=="Uber"){
+		if(uberState == 0){
+			uberState = 1;
+		}else{
+			uberState = 0;
+		}
+	}
+	
+};
+
+
+
